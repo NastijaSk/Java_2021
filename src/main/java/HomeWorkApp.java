d29721be905875716e84d6037293416f7f3a62c3
Nastija <nastijaa@mail.ru>
2021-12-29 12:24:35 +0300

    первое практическое задание. форматирование кода

--- src/main/java/HomeWorkApp.java
+++ src/main/java/HomeWorkApp.java
@@ -9,48 +9,53 @@ public class HomeWorkApp {
         printColor(100);
         printColor(101);
         compareNumbers(10, 20);
-        compareNumbers(10 ,-5);
+        compareNumbers(10, -5);
     }
-    
-    public static void printThreeWords()
-    {
+
+    public static void printThreeWords() {
         System.out.println("Orange\n" +
                 "Banana\n" +
                 "Apple");
     }
 
-    public static void checkSumSign()
-    { int a = 10, b = -20;
-        if (a + b>=0) {
-         System.out.println("Сумма положительная");}
-            else {System.out.println("Сумма отрицательная");}
+    public static void checkSumSign() {
+        int a = 10, b = -20;
+        if (a + b >= 0) {
+            System.out.println("Сумма положительная");
+        } else {
+            System.out.println("Сумма отрицательная");
+        }
 
     }
 
-    public static void printColor( int value)
-    {
+    public static void printColor(int value) {
 
        /* Если value меньше 0 (0 включительно), то в консоль метод должен вывести сообщение “Красный”,
         если лежит в пределах от 0 (0 исключительно) до 100 (100 включительно), то “Желтый”,
         если больше 100 (100 исключительно) - “Зеленый”; */
-        if (value<=0) { System.out.println("Красный");}
-        else if (value >0 && value <=100) {System.out.println("Желтый");}
-        else {System.out.println("Зеленый");}
+        if (value <= 0) {
+            System.out.println("Красный");
+        } else if (value > 0 && value <= 100) {
+            System.out.println("Желтый");
+        } else {
+            System.out.println("Зеленый");
+        }
 
     }
 
-    public static void compareNumbers(int a, int b)
-    {
+    public static void compareNumbers(int a, int b) {
         /*Если a больше или равно b, то необходимо вывести в консоль сообщение “a >= b”,
          в противном случае “a < b
          */
-        if (a>=b ){print_a("a >= b");}
-        else {print_a("a < b");}
+        if (a >= b) {
+            print_a("a >= b");
+        } else {
+            print_a("a < b");
+        }
 
     }
 
-    public static void print_a(String a)
-    {
+    public static void print_a(String a) {
         System.out.println(a);
     }
 }
